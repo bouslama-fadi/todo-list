52752fb206168c288372681ec5e34336678ca0cb
Rami Boudaya <fedy.bouslama@gmail.com>
2021-04-10 01:09:34 +0100

    modal creation - next nav bar

--- src/components/elements/Todo.js
+++ src/components/elements/Todo.js
@@ -1,7 +1,12 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import { Button, ListGroup, ListGroupItem, Badge } from "reactstrap";
 
+import DeleteTaskModal from "../modals/DeleteTaskModal";
+
 const Todo = ({ text, description, todo, todos, setTodos }) => {
+  const [modal, setModal] = useState(false);
+  const toggle = () => setModal(!modal);
+
   const deleteHandler = () => {
     setTodos(todos.filter((el) => el.id !== todo.id));
   };
@@ -37,10 +42,16 @@ const Todo = ({ text, description, todo, todos, setTodos }) => {
             <Button
               style={{ marginLeft: 5 }}
               color="danger"
-              onClick={deleteHandler}
+              // onClick={deleteHandler}
+              onClick={toggle}
             >
               Delete
             </Button>
+            <DeleteTaskModal
+              modal={modal}
+              toggle={toggle}
+              deleteHandler={deleteHandler}
+            />
           </div>
           {todo.completed ? (
             <div className="badgeStyle">
--- src/components/modals/DeleteTaskModal.js
+++ src/components/modals/DeleteTaskModal.js
@@ -1,7 +1,23 @@
 import React, { Component } from "react";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 
-const DeleteTaskModal = () => {
-  return <div>this is my modal</div>;
+const DeleteTaskModal = ({ modal, toggle, deleteHandler }) => {
+  return (
+    <div>
+      <Modal isOpen={modal} toggle={toggle}>
+        <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+        <ModalBody>Are you sure you want to delete this item ?</ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={deleteHandler}>
+            Confirm
+          </Button>
+          <Button color="secondary" onClick={toggle}>
+            Cancel
+          </Button>
+        </ModalFooter>
+      </Modal>
+    </div>
+  );
 };
 
 export default DeleteTaskModal;
